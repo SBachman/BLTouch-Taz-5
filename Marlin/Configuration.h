753bc0cf196b898249eb81489dad87bff1e8063b
Shayne <menissalt@gmail.com>
2019-01-23 03:43:12 -0600

    increase mesh grid
    No longer using G29 in start gcode. This is working MUCH better and only requires abl to run when the printer is turned on rather than at the begining of each print. May increase further.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1107,8 +1107,8 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
-  #define GRID_MAX_POINTS_Y 4
+  #define GRID_MAX_POINTS_X 6
+  #define GRID_MAX_POINTS_Y 6
 
   // Set the boundaries for probing (where the probe can reach).
   #define LEFT_PROBE_BED_POSITION 20
