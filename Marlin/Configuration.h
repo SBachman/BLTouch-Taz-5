431d176f57c13e2c01f8634abe20fe72b41f10a2
SBachman <36245316+SBachman@users.noreply.github.com>
2019-05-04 18:19:36 -0500

    Set probe to 9x9 and expanded boundries
    Run G28, G29, then G500 to save mesh to eeprom. In start gcode use M420 S1 instead of G29. This allows for more accurate leveling and less startup time for each print

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1107,14 +1107,14 @@
 #if ENABLED(AUTO_BED_LEVELING_LINEAR) || ENABLED(AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 6
-  #define GRID_MAX_POINTS_Y 6
+  #define GRID_MAX_POINTS_X 9
+  #define GRID_MAX_POINTS_Y 9
 
   // Set the boundaries for probing (where the probe can reach).
-  #define LEFT_PROBE_BED_POSITION 20
-  #define RIGHT_PROBE_BED_POSITION 260
-  #define FRONT_PROBE_BED_POSITION 20
-  #define BACK_PROBE_BED_POSITION 240
+  #define LEFT_PROBE_BED_POSITION 5
+  #define RIGHT_PROBE_BED_POSITION 275
+  #define FRONT_PROBE_BED_POSITION 5
+  #define BACK_PROBE_BED_POSITION 245
 
   // Probe along the Y axis, advancing X after each column
   #define PROBE_Y_FIRST LULZBOT_PROBE_Y_FIRST
